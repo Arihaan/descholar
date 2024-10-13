33377a1f5be7ac2c33b7db17829d9cacb90ace86
Arihaan <48653895+Arihaan@users.noreply.github.com>
2024-10-13 09:48:17 +0100

    changes

--- stellarshipFn/stellarshipfrontend/app/components/Navbar.tsx
+++ stellarshipFn/stellarshipfrontend/app/components/Navbar.tsx
@@ -18,6 +18,9 @@ const Navbar = () => {
             <Link href="/addscholarship" className="text-gray-300 hover:text-blue-400 px-3 py-2 rounded-md text-sm font-medium">
               Create Scholarship
             </Link>
+            <Link href="/addscholarship" className="text-gray-300 hover:text-blue-400 px-3 py-2 rounded-md text-sm font-medium">
+              My Applications
+            </Link>
             <ConnectButton label="Connect Wallet" />
           </div>
         </div>
