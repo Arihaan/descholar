8a6738c5ed2550fd821222f99c44698c4b5e264a
f1l1ph <filipmasarik04@gmail.com>
2024-10-13 02:23:43 +0200

    everything is broken

--- stellarshipFn/stellarshipfrontend/app/addscholarship/page.tsx
+++ stellarshipFn/stellarshipfrontend/app/addscholarship/page.tsx
@@ -1,66 +1,112 @@
 "use client";
-import { Scholarship } from "bindings";
+import { Client, networks, Scholarship } from "bindings";
 import React from "react";
+import { kit } from "../stellar-wallets-kit";
+import { WalletNetwork } from "@creit.tech/stellar-wallets-kit";
 
 const createScholarshipPage = () => {
+  const scholarshipContract = new Client({
+    contractId: networks.testnet.contractId,
+    networkPassphrase: networks.testnet.networkPassphrase,
+    rpcUrl: "https://soroban-testnet.stellar.org/",
+  });
+
   const [scholarship, setScholarship] = React.useState<Scholarship>();
 
-  return (
-    <>
-      <div className="flex justify-center items-start mt-32">
-        <h2 className="font-extrabold text-5xl">Create new Stellarship</h2>
-      </div>
-      <div className="flex justify-center items-start mt-12">
-        <div className="card bg-neutral text-neutral-content w-96">
-          <div className="card-body items-center text-center">
-            <form className="place-content-center ">
-              <input
-                type="text"
-                placeholder="Stellarship Name"
-                className="input input-bordered w-full max-w-xs m-2"
-                value={scholarship?.name}
-              />
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    // handle change logic here
+  }
 
-              <input
-                type="text"
-                placeholder="Stellarship Details"
-                className="input input-bordered w-full max-w-xs m-2"
-                value={scholarship?.details}
-              />
+  async function submitScholarship() {
+    const { address } = await kit.getAddress();
 
-              <input
-                type="text"
-                placeholder="Available Grants"
-                className="input input-bordered w-full max-w-xs m-2"
-                value={scholarship?.available_grants.toString()}
-              />
+    if (!address) {
+      alert("Please connect your wallet first");
+      return;
+    } else {
+      if (scholarship) {
+        scholarshipContract.post_scholarship({ scholarship });
+      } else {
+        alert("Scholarship details are incomplete.");
+      }
+    }
 
-              <input
-                type="text"
-                placeholder="Total Grant Amount"
-                className="input input-bordered w-full max-w-xs m-2"
-                value={scholarship?.total_grant_amount.toString()}
-              />
+    try {
+      if (scholarship) {
+        const { signedTxXdr } = await kit.signTransaction(
+          scholarship.toString(),
+          {
+            address,
+            networkPassphrase: WalletNetwork.PUBLIC,
+          }
+        );
+      } else {
+        throw new Error("Scholarship details are incomplete.");
+      }
+    } catch (e) {
+      console.error(e);
+    }
 
-              <input
-                type="text"
-                placeholder="End Date"
-                className="input input-bordered w-full max-w-xs m-2"
-                value={scholarship?.end_date.toString()}
-              />
-            </form>
+    return (
+      <>
+        <div className="flex justify-center items-start mt-32">
+          <h2 className="font-extrabold text-5xl">Create new Stellarship</h2>
+        </div>
+        <div className="flex justify-center items-start mt-12">
+          <div className="card bg-primary text-neutral-content max-w-lg">
+            <div className="card-body items-center text-center">
+              <form
+                className="place-content-center "
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  submitScholarship();
+                }}
+              >
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className="input input-bordered w-full max-w-xs m-2"
+                  value={scholarship?.name}
+                />
+
+                <input
+                  type="text"
+                  placeholder="Details"
+                  className="input input-bordered w-full max-w-xs m-2"
+                  value={scholarship?.details}
+                />
+
+                <input
+                  type="text"
+                  placeholder="Available Grants"
+                  className="input input-bordered w-full max-w-xs m-2"
+                  value={scholarship?.available_grants.toString()}
+                />
+
+                <input
+                  type="text"
+                  placeholder="Total Grant Amount $"
+                  className="input input-bordered w-full max-w-xs m-2"
+                  value={scholarship?.total_grant_amount.toString()}
+                />
+
+                <input
+                  type="date"
+                  placeholder="End Date"
+                  className="input input-bordered w-full max-w-xs m-2"
+                  value={scholarship?.end_date.toString()}
+                />
+
+                <button type="submit" onClick={submitScholarship}>
+                  Submit
+                </button>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 };
-// export interface Scholarship {
-//   available_grants: u32;
-//   details: string;
-//   end_date: u64;
-//   name: string;
-//   total_grant_amount: i128;
-// }
 
 export default createScholarshipPage;
